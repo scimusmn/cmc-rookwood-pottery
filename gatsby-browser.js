b9fae99f8455fabf5ef892db31ba25c69eca6692
trig-vee <tnordberg@smm.org>
2022-05-30 12:33:36 -0500

    feat: load local fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 // Don't require a default export. Gatsby's API can't support it here.
 import PropTypes from 'prop-types';
-import './src/styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './src/styles/index.css';
 
 export const wrapRootElement = ({ element }) => element;
 
