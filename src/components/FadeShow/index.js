a230ed458df68041582ce014d4cb615d5a071714
trig-vee <tnordberg@smm.org>
2022-05-30 12:33:55 -0500

    feat: load local fonts

---
+++ src/components/FadeShow/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import useInterval from 'use-interval';
+import PropTypes from 'prop-types';
+
+function FadeShow({ delay, elements }) {
+  const [fadeIndex, setFadeIndex] = useState(0);
+
+  useInterval(() => {
+    let nextIndex = fadeIndex + 1;
+    if (nextIndex >= elements.length) nextIndex = 0;
+    console.log('nextIndex', fadeIndex, nextIndex, elements.length);
+    setFadeIndex(nextIndex);
+  }, delay);
+
+  return (
+    <div className="fade-show">
+      {elements.map((element, index) => (
+        <p
+          key={element}
+          className={`fade-show-element ${(index === fadeIndex ? 'show' : '')}`}
+        >
+          {element}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+FadeShow.defaultProps = {
+  delay: 4000,
+  elements: [],
+};
+
+FadeShow.propTypes = {
+  delay: PropTypes.number,
+  elements: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default FadeShow;
