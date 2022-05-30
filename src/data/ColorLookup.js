171600258dcb8ba1eba29a8c2dfb099a87d91817
trig-vee <tnordberg@smm.org>
2022-05-29 23:19:53 -0500

    feat: color lookups

---
+++ src/data/ColorLookup.js
@@ -0,0 +1,10 @@
+const COLOR_LOOKUP = {
+  Tomato: { before: '#bb6347', after: '#ff6347' },
+  teal: { before: '#004040', after: '#008080' },
+  gold: { before: '#bb8700', after: '#ffd700' },
+  DarkSeaGreen: { before: '#1e4b27', after: '#2e8b57' },
+  DarkSlateGrey: { before: '#203050', after: '#708090' },
+  DarkGoldenRod: { before: '#68360b', after: '#b8860b' },
+};
+
+export default COLOR_LOOKUP;
